452332365cc4f05ac1e9b3d9a611420be28a41bb
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2024-01-26 19:48:01 +0000

    update baseURL for deployment

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://spannerbackend-ancient-snowflake-7021.fly.dev',
     withCredentials: true,
     // params: {
     //     key: 'session_id=90a2ee22-e839-4acf-9733-5c7de1122ed6',
